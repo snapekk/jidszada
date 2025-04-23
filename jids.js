5d2ac4765ba99eb76745a5da3e5188a4765f6f1d
snapekk <126613007+snapekk@users.noreply.github.com>
2025-04-23 15:42:53 -0300

    Delete jids.js

--- jids.js
+++
@@ -1,9 +0,0 @@
-[
-  "558385528473@s.whatsapp.net",
-  "550297342838@s.whatsapp.net",
-  "554942800624@s.whatsapp.net",
-  "550606215552@s.whatsapp.net",
-  "551360392961@s.whatsapp.net",
-  "559595724162@s.whatsapp.net",
-  "553733241003@s.whatsapp.net"
-];
